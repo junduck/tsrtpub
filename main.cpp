a0d71b271772d1183492f25162d009dd8b2ec683
Jun <imlijunda@gmail.com>
2020-08-27 17:54:48 +0930

    embed usage

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include "ixwebsocket/IXNetSystem.h"
 #include "tsrt_pub/tsrt_pub.hpp"
 
-int main(int, char**) {
+int main(int, char **)
+{
   // required on Windows
   ix::initNetSystem();
 
-  tsrt::tsrt_pub pub("YOUR TUSHARE TOKEN");
+  tsrt::tsrt_pub pub("YOUR TUSHARE TOKEN",
+                     "YOUR REALTIME TOPIC, e.g. HQ_STK_TICK",
+                     {"YOUR SUBSCRIBED CODES, AS A VECTOR OF STRING, e.g. {*}"},
+                     "wss://ws.waditu.com/listening",
+                     "YOUR PUBLISHER BINDING ADDRESS, e.g. tcp://*:6667");
   pub.run();
 
   ix::uninitNetSystem();
